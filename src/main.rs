6a4fc0eba409fd546999ab0d4324cdac8b8496ca
Zaid-maker <pzhafeez@gmail.com>
2023-01-05 13:33:12 +0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
     println!("Please input your guess.");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Equal => println!("Too win!"),
+        Ordering::Greater => println!("Too big!"),
+    }
 
     let mut guess = String::new();
 
